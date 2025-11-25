c46a25d72eb2939a51dae2cc8ad7631b3187833a
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-11-25 21:02:32 +0530

    feat: Enable SuperJSON for tRPC client, server, and React Query data serialization.

--- trpc/client.tsx
+++ trpc/client.tsx
@@ -2,6 +2,7 @@
 // ^-- to make sure we can mount the Provider from a server component
 import { useState } from "react";
 
+import superjson from "superjson";
 import type { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
@@ -9,8 +10,11 @@ import { createTRPCContext } from "@trpc/tanstack-react-query";
 
 import type { AppRouter } from "./routers/_app";
 import { makeQueryClient } from "./query-client";
+
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
+
 let browserQueryClient: QueryClient;
+
 function getQueryClient() {
   if (typeof window === "undefined") {
     // Server: always make a new query client
@@ -23,6 +27,7 @@ function getQueryClient() {
   if (!browserQueryClient) browserQueryClient = makeQueryClient();
   return browserQueryClient;
 }
+
 function getUrl() {
   const base = (() => {
     if (typeof window !== "undefined") return "";
@@ -30,6 +35,7 @@ function getUrl() {
   })();
   return `${base}/api/trpc`;
 }
+
 export function TRPCReactProvider(
   props: Readonly<{
     children: React.ReactNode;
@@ -44,7 +50,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
