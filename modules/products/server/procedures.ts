7dcfaf80dbc4540a31da76be3d8aca3bd7ade51a
Bhushan Lagare <bhushanlagare7@gmail.com>
2025-11-27 23:38:56 +0530

    feat: Implement product tags, product sorting, and advanced product filtering by price and tags.

--- modules/products/server/procedures.ts
+++ modules/products/server/procedures.ts
@@ -1,8 +1,10 @@
-import { Where } from "payload";
+import { Sort, Where } from "payload";
 import { z } from "zod";
 
 import { Category } from "@/payload-types";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+
+import { sortValues } from "../search-params";
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
@@ -11,17 +13,35 @@ export const productsRouter = createTRPCRouter({
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
+        tags: z.array(z.string()).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
+      let sort: Sort = "-createdAt";
 
-      if (input.minPrice) {
-        where["price"] = { greater_than_equal: Number(input.minPrice) };
+      if (input.sort === "curated") {
+        sort = "-createdAt";
       }
 
-      if (input.maxPrice) {
-        where["price"] = { less_than_equal: Number(input.maxPrice) };
+      if (input.sort === "hot_and_new") {
+        sort = "+createdAt";
+      }
+
+      if (input.sort === "trending") {
+        sort = "-createdAt";
+      }
+
+      if (input.minPrice && input.maxPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = { greater_than_equal: input.minPrice };
+      } else if (input.maxPrice) {
+        where.price = { less_than_equal: input.maxPrice };
       }
 
       if (input.category) {
@@ -58,10 +78,17 @@ export const productsRouter = createTRPCRouter({
         }
       }
 
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags,
+        };
+      }
+
       const products = await ctx.db.find({
         collection: "products",
         depth: 1, // Populate "category" & "image" fields
         where,
+        sort,
       });
 
       return products;
